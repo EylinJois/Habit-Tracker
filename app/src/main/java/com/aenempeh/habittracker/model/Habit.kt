280c2146352db9e056ab1e559842afd902375bad
EylinJois <s160423031@student.ubaya.ac.id>
2026-07-06 11:36:13 +0700

    Database setup done

--- app/src/main/java/com/aenempeh/habittracker/model/Habit.kt
+++ app/src/main/java/com/aenempeh/habittracker/model/Habit.kt
@@ -1,12 +1,24 @@
 package com.aenempeh.habittracker.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import androidx.room.ColumnInfo
+
+@Entity
 data class Habit(
-    val id: String,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    @ColumnInfo(name="name")
     val name: String,
+    @ColumnInfo(name="desc")
     val description: String,
+    @ColumnInfo(name="goal")
     val goal: Int,
+    @ColumnInfo(name="unit")
     val unit: String,
+    @ColumnInfo(name="icon")
     val icon: String,
+    @ColumnInfo(name="count")
     var currentCount: Int = 0
 ) {
     fun isCompleted() = currentCount >= goal
---
+++ app/src/main/java/com/aenempeh/habittracker/model/HabitDatabase.kt
@@ -0,0 +1,32 @@
+package com.aenempeh.habittracker.model
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(Habit::class), version =  1)
+abstract class HabitDatabase: RoomDatabase() {
+    abstract fun habitDao(): HabitDao
+
+    companion object {
+        @Volatile private var instance: HabitDatabase ?= null
+        private val LOCK = Any()
+
+        fun buildDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                HabitDatabase::class.java,
+                "newtododb").build()
+
+        operator fun invoke(context:Context) {
+            if(instance != null) {
+                synchronized(LOCK) {
+                    instance ?: buildDatabase(context).also {
+                        instance = it
+                    }
+                }
+            }
+        }
+    }
+}
